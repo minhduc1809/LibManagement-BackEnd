9a8b39567d77cda785aaa5dbeb8aeb96e49db4af
minhduc1809 <119800775+minhduc1809@users.noreply.github.com>
2025-11-05 15:10:24 +0700

    kết thúc phần Đức

--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
-    
+    // Trong UserRepository.java
     Optional<UserAccount> findByUsername(String username);
     
     Optional<UserAccount> findByEmail(String email);
